9e1ea3b8677237731069df00bfcca94fd3ce081e
Amanda Mbatha <amanda.mbatha@strathmore.edu>
2024-09-06 13:46:10 +0300

    conditionals

---
+++ api 2.2a/conditionals.js
@@ -0,0 +1,15 @@
+let colour = 'red';
+switch(colour){
+    case 'blue':
+        console.log("Blue");
+        break;
+    case 'red':
+        console.log("Red");
+        break;
+    case 'yellow':
+        console.log("Yellow");
+        break;
+    default:
+        console.log("Not Valid");
+        break;
+}
